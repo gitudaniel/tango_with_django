4129d9e6f15ddc373acc06a6f26fc9fdf6eb6096
Daniel Gitu <gitudaniel@gmail.com>
2017-04-19 10:48:47 +0300

    Added links to other views

--- rango/urls.py
+++ rango/urls.py
@@ -2,5 +2,7 @@ from django.conf.urls import url
 from rango import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.home, name='home'),
+    url(r'^index/$', views.index, name='index'),
+    url(r'^about/$', views.about, name='about'),
 ]
--- rango/views.py
+++ rango/views.py
@@ -4,6 +4,15 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 from django.http import HttpResponse
 
+def home(request):
+    return HttpResponse("Rango says welcome partner!!")
+
 def index(request):
-    return HttpResponse("Rango says hey there partner!")
+    html1 = "Rango says hey there partner!" '<a href="/rango/about">About</a>'
+    return HttpResponse(html1)
+
+def about(request):
+    html2 = "Rango says here is the about page" '<a href="/rango/">Home</a>'
+
+    return HttpResponse(html2)
 
