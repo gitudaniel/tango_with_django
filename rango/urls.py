8b11cb23d532c87a473cbe3de75dda5ecf145bf1
Daniel Gitu <gitudaniel@gmail.com>
2017-05-22 18:35:57 +0300

    Added bootstrap styling and widget tweaks forms

--- rango/urls.py
+++ rango/urls.py
@@ -5,10 +5,7 @@ urlpatterns = [
     url(r'^$', views.home, name='home'),
     url(r'^about/$', views.about, name='about'),
     url(r'^add_category/$', views.add_category, name='add_category'),
-    url(r'^register/$', views.register, name='register'),
-    url(r'^login/$', views.user_login, name='login'),
     url(r'^restricted/$', views.restricted, name='restricted'),
-    url(r'^logout/$', views.user_logout, name='logout'),
 
     url(r'^category/(?P<category_name_slug>[\w\-]+)/$',
         views.show_category,
